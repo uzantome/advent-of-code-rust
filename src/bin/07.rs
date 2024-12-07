258557d68cc0bdaea18240f951974a2d485abc12
uzanto <git@uzanto.me>
2024-12-07 22:34:56 +0100

    add Day 7 Part Two

--- src/bin/07.rs
+++ src/bin/07.rs
@@ -43,8 +43,57 @@ fn is_solvable(test_value: u64, numbers: &[u32], index: usize) -> bool {
     false
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+/// Checks whether the test value can be solved by combining the given numbers (in the given order) with ADD, MUL or CONCAT operations.
+/// works in the reverse way as is_solvable
+fn is_solvable_2(test_value: u64, numbers: &[u32], index: usize, curr: u64) -> bool {
+    if curr > test_value {
+        return false;
+    }
+
+    if index == numbers.len() {
+        return curr == test_value;
+    }
+
+    let a = numbers[index];
+    let i = index + 1;
+
+    let t = curr + a as u64;
+    if is_solvable_2(test_value, numbers, i, t) {
+        return true;
+    }
+
+    let t = curr * a as u64;
+    if is_solvable_2(test_value, numbers, i, t) {
+        return true;
+    }
+
+    let concat = format!("{}{}", curr, a);
+    let t = concat.parse().unwrap();
+    if is_solvable_2(test_value, numbers, i, t) {
+        return true;
+    }
+
+    false
+}
+
+pub fn part_two(input: &str) -> Option<u64> {
+    let equations: Vec<(u64, Vec<u32>)> = input
+        .lines()
+        .map(|line| {
+            let parts: Vec<&str> = line.split(": ").collect();
+            let test_value: u64 = parts[0].parse().unwrap();
+            let numbers: Vec<u32> = parts[1].split(" ").map(|x| x.parse().unwrap()).collect();
+            (test_value, numbers)
+        })
+        .collect();
+
+    let sum: u64 = equations
+        .iter()
+        .filter(|(test_value, numbers)| is_solvable_2(*test_value, numbers, 0, 0))
+        .map(|(test_value, _)| test_value)
+        .sum();
+
+    Some(sum)
 }
 
 #[cfg(test)]
@@ -60,6 +109,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(11387));
     }
 }
