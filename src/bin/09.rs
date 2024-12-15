5bbb1434451e1dcd81f2d88670474d7c987af4e5
uzanto <git@uzanto.me>
2024-12-15 01:17:13 +0100

    add Day 9 Part Two

--- src/bin/09.rs
+++ src/bin/09.rs
@@ -43,8 +43,105 @@ pub fn part_one(input: &str) -> Option<u64> {
     Some(checksum)
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<u64> {
+    let mut blocks: Vec<(i32, u32)> = input
+        .trim()
+        .chars()
+        .filter_map(|c| c.to_digit(10))
+        .enumerate()
+        .map(|(i, size)| {
+            if i % 2 == 0 {
+                // file
+                (i as i32 / 2, size)
+            } else {
+                // free space
+                (-1, size)
+            }
+        })
+        .collect();
+
+    // for i in (0..blocks.len()).rev() {
+    let mut i = blocks.len() - 1;
+    while i > 0 {
+        if blocks[i].0 == -1 {
+            // skip free space
+            i -= 1;
+            continue;
+        }
+
+        let filesize = blocks[i].1;
+
+        let mut j = 0;
+        while j < i {
+            if blocks[j].0 != -1 {
+                // skip files
+                j += 1;
+                continue;
+            }
+
+            let freesize = blocks[j].1;
+
+            if freesize < filesize {
+                j += 1;
+                continue;
+            }
+
+            // found gap: move file
+            blocks[j] = blocks[i];
+
+            // move "displaced" free space to moved file's old position
+            blocks[i] = (-1, filesize);
+            if (i + 1) < blocks.len() && blocks[i + 1].0 == -1 {
+                blocks[i].1 += blocks[i + 1].1;
+                blocks.remove(i + 1);
+            }
+            if blocks[i - 1].0 == -1 {
+                blocks[i - 1].1 += blocks[i].1;
+                blocks.remove(i);
+            }
+
+            // if free space is left after moving file, extend existing free block or
+            // insert new one
+            let slacksize = freesize - filesize;
+            if slacksize > 0 {
+                if blocks[j + 1].0 == -1 {
+                    blocks[j + 1].1 += slacksize;
+                } else {
+                    blocks.insert(j + 1, (-1, slacksize));
+                }
+            }
+
+            // stop looking for gaps for this file
+            break;
+        }
+
+        i -= 1;
+    }
+
+    let sorted_map: Vec<i32> = blocks
+        .into_iter()
+        .flat_map(|(id, size)| {
+            if id == -1 {
+                std::iter::repeat(-1)
+                    .take(size as usize)
+                    .collect::<Vec<_>>()
+            } else {
+                std::iter::repeat(id)
+                    .take(size as usize)
+                    .collect::<Vec<_>>()
+            }
+        })
+        .collect();
+
+    let checksum: u64 = sorted_map.iter().enumerate().fold(0, |acc, (pos, &id)| {
+        if id == -1 {
+            acc
+        } else {
+            acc + pos as u64 * id as u64
+        }
+    });
+
+    Some(checksum)
 }
 
 #[cfg(test)]
@@ -60,6 +157,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, Some(1928));
+        assert_eq!(result, Some(2858));
     }
 }
